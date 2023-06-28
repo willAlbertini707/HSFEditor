bd4a8952d0c500ef5d4975a084c07e87c59a6bf5
willAlbertini707 <willalbertini707@gmail.com>
2023-06-28 15:12:18 -0700

    First commit. Unfinised index page

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+from threading import Timer
+import webbrowser
+
+app = Flask(__name__, template_folder='templates', static_folder='styling')
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/result", methods = ["POST", "GET"])
+def result():
+    return "<h1>Hello</h1>"
+
+def open_browser():
+  webbrowser.open_new("http://127.0.0.1:5000")
+
+if __name__ == "__main__":
+    Timer(1, open_browser).start()
+    app.run()
